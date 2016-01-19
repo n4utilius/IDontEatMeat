9e34b5994954130c1568415a5294e753d6e32d98
Kevin Martinez <n4utilius@gmail.com>
2016-01-19 11:57:48 -0600

    Refactoring login method

---
+++ keys2.py
@@ -0,0 +1,9 @@
+keys = [
+    dict(
+        screen_name= 'n4utibot',
+        consumer_key= 'DzeB8MLAuekG9DdbQggKJKXYi',
+        consumer_secret= 'Ln80yqqHUbcYfcmDzFqPVuAOx7BSFulI1PlMkIwSVB3Xjpf7Bk',
+        access_token =  '1014648932-fKm7gMy92NjL7IgWg59lcBBhVrnUWQd2p2hobXm',
+        access_token_secret = 'zZkZIb77NMxHVM8QAJDCcMWIdalnGvrch13gTfEU0e6qD',
+    )
+]
